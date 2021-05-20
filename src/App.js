0ee7069f431260a2002423737ccddd6f81e2b7a5
boo9305 <zoflr9305@naver.com>
2021-05-20 01:26:38 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Rect from './Rect'
 
 function App() {
+  const [viewBox, setViewBox] = useState("")
+  const [vbX, setVbX] = useState(600)
+
+  const onSvgClick = (e) => {
+    //alert(e.target.id)
+    if (e.target.id === "rect") {
+//      if (box.strokeDasharray !== 0) setBox({...box, strokeDasharray : 0 , r : 0, cursor : "default"})
+//      else setBox({...box, strokeDasharray : 2 , r : 4, cursor : "move"})
+    }
+    else {
+//      const x = 50i
+//      setViewBox("0 0 " + String(vbX - x) + " " + String(vbX - x))
+//      setVbX(vbX - x)
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Svg" >
+        <svg viewBox={viewBox} width="600" height="600" style={{ border : "1px solid gray"}} >
+          <Rect></Rect>
+        </svg>
+
+      </div>
     </div>
   );
 }
